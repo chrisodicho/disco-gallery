57208b4bc1446b9516824855a74562b4e5ce4507
Chris Odicho <chris.odicho@gmail.com>
2024-08-14 09:30:51 +1000

    feat: add placeholder loader for artwork route

--- src/routes/artwork.tsx
+++ src/routes/artwork.tsx
@@ -1,3 +1,17 @@
+export async function loader({
+  params,
+}: {
+  params: {
+    id?: string;
+  };
+}) {
+  if (params.id) {
+    // fetch data and return artwork
+  }
+
+  return { artwork: null };
+}
+
 export function Artwork() {
   return <div>{'Artwork route'}</div>;
 }
